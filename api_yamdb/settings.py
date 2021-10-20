a97bb769b506c6ac4816f0fddb12514be4bd1a07
Kirill1709 <kirill.gatsuk13@gmail.com>
2021-10-20 03:08:37 +0300

    add secret key to settings

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -1,24 +1,15 @@
 import os
 from datetime import timedelta
 
-import environ
-
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-DEBUG = env('DEBUG')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
