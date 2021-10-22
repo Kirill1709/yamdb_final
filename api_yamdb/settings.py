d712c409f329109640be8342aa2d5e1c1e33348b
Kirill1709 <kirill.gatsuk13@gmail.com>
2021-10-23 00:02:38 +0300

    default hosts

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = os.environ('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', False)
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'jknkjsdfnsdjfns')
 
