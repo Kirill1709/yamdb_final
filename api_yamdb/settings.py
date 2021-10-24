af2f33eff09c954f1ba3d62e8e033ceafa4a4b88
Kirill1709 <kirill.gatsuk13@gmail.com>
2021-10-25 02:03:20 +0300

    fix ALLOWED_HOSTS

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'jknkjsdfnsdjfns')
 
 print(SECRET_KEY)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', ['*'])
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 
 print(ALLOWED_HOSTS)
 
